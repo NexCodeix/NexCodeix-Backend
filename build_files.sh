c0b1172ec5f2562961b18a4e3b27131b26ba7a33
Suhaib Safwan <nexcodeix@gmail.com>
2024-12-26 02:40:56 +0300

    update build file

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,7 @@
-pip3 install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+echo "Now will install packages"
+python3 -m pip install -r requirements.txt
+echo "Package installed"
+
+echo "Collecting static folder"
+python3 manage.py collectstatic --noinput
+echo "Collected"
