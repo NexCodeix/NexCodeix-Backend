ae72721720da742a8f6fcd987882e03b6ca41744
Suhaib Safwan <nexcodeix@gmail.com>
2024-12-26 01:43:49 +0300

    init proj

---
+++ main/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+
+class Portfolio(models.Model):
+    title = models.CharField(
+        max_length=100,
+    )
+    description = models.TextField()
+    profile_image = models.ImageField(
+        upload_to="portfolio/profile_image/"
+    )
+    date_created = models.DateTimeField(
+        auto_now_add=True,
+    )
+    last_updated = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class PortfolioContent(models.Model):
+    portfolio = models.ForeignKey(
+        Portfolio,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    title = models.CharField(
+        max_length=200,
+    )
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class PortfolioImages(models.Model):
+    portfolio = models.ForeignKey(
+        Portfolio,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    title = models.CharField(
+        max_length=200,
+    )
+    profile_image = models.ImageField(
+        upload_to="portfolio/images/"
+    )
+
+    def __str__(self):
+        return self.title
